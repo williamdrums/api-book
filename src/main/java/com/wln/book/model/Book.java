b71c85bef654da9efdfc683c5ac7547ea266b27e
williamnascimento <Kr99u4U9w2KY9yn>
2021-08-06 09:56:22 -0400

    adicionando lombok  para geração de get and setter,constructor and hash code and equals

--- src/main/java/com/wln/book/model/Book.java
+++ src/main/java/com/wln/book/model/Book.java
@@ -1,10 +1,22 @@
 package com.wln.book.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-public class Book {
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,58 +28,4 @@ public class Book {
     @Column(nullable = false)
     private String author;
 
-    public Book() {
-    }
-
-    public Book(Long id, String title, String author) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, author);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                '}';
-    }
 }
